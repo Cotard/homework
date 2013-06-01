93339b9c825971293a8afac7f391f802d1f3a14b
Cotard <aveanorexia@gmail.com>
2013-06-01 17:53:17 +0400

    some changes

--- hw1/js/my.js
+++ hw1/js/my.js
@@ -1,8 +1,17 @@
 var $add_comment = $(".add_comment").clone();
 
+$('.foot_comments a').click(function(event){
+   	event.stopPropagation();
+   	$add_comment.children("textarea").val('');
+	$add_comment.appendTo($(this).parent().parent());
+   	$add_comment.show("fast");
+});
 
-function answer(id) {
-	var $block = "#" + id;
-	$add_comment.children("textarea").val('');
-	$add_comment.appendTo($($block));
-}
+$(function(){
+	$(document).click(function(event) {
+    	if ($(event.target).closest($add_comment).length) 
+    		return;
+    	$add_comment.hide("fast");
+    	event.stopPropagation();
+  });
+});
