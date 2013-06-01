2aec8c3da397fa1e40c452e1c97823793a539edd
Cotard <aveanorexia@gmail.com>
2013-06-01 18:14:56 +0400

    some changes

--- hw1/js/my.js
+++ hw1/js/my.js
@@ -3,8 +3,14 @@ var $add_comment = $(".add_comment").clone();
 $('.foot_comments a').click(function(event){
    	event.stopPropagation();
    	$add_comment.children("textarea").val('');
-	$add_comment.appendTo($(this).parent().parent());
-   	$add_comment.show("fast");
+
+	var list = $(this).find('div');
+	if(list.length > 0)
+		$add_comment.toggle("fast");
+   	else
+   		//$add_comment.hide());
+		$add_comment.appendTo($(this).parent().parent());
+   
 });
 
 $(function(){
